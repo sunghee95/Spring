7cd3df1453a90347b89b3b957e1383fa4cb35c6a
sunghee95 <sunghee9647@gmail.com>
2023-05-31 17:26:41 +0900

    Spring Form 태그 숨겨진 필드 요소 사용 예제 Controller

---
+++ DevProject/src/main/java/kr/or/ddit/controller/form/hidden/JSPFormHiddenTagController.java
@@ -0,0 +1,41 @@
+package kr.or.ddit.controller.form.hidden;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.or.ddit.vo.Member;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping("/formtag/hidden")
+@Slf4j
+public class JSPFormHiddenTagController {
+	
+	/*
+	 * 8장 스프링 폼 태그 
+	 * 
+	 *  12. 숨겨진 필드 요소 
+	 *  -HTML 숨겨진 필드를 출력하려면 <form:hidden> 요소를 사용한다 
+	 */
+	
+	@RequestMapping(value = "/registerFormHidden01", method = RequestMethod.GET)
+	public String registerFormHidden01(Model model) {
+		log.info("registerFormHidden01() 실행!");
+		Member member = new Member();
+		member.setUserId("cho123");
+		member.setUserName("성히");
+		model.addAttribute("member", member);
+		return "form/hidden/registerFormHidden01";		
+	}
+	
+	@RequestMapping(value = "/result", method = RequestMethod.POST)
+	public String registerFormHiddenResult(Member member, Model model) {
+		log.info("registerFormHiddenResult() 실행!");
+		log.info("userId : " + member.getUserId());
+		log.info("userName : " + member.getUserName());
+		//model.addAttribute(attributeName, attributeValue)
+		return "form/hidden/result";	
+	}	
+}
