09c55dac051a3740f0c1b3843db8bab11cb72bc8
sunghee95 <sunghee9647@gmail.com>
2023-06-07 17:14:26 +0900

    파일 업로드를 위한 vo

---
+++ DevProject/src/main/java/kr/or/ddit/vo/Item3.java
@@ -0,0 +1,13 @@
+package kr.or.ddit.vo;
+
+import lombok.Data;
+
+@Data
+public class Item3 {
+	private int itemId;
+	private String itemName;
+	private int price;
+	private String description;
+	private String[] files;
+	
+}
