4a72cc800417198774ed213f16937a1795a4d741
sunghee95 <sunghee9647@gmail.com>
2023-06-07 17:16:56 +0900

    파일업로드를 위한 mapper

--- DevProject/src/main/java/kr/or/ddit/mapper/NoticeMapper.java
+++ DevProject/src/main/java/kr/or/ddit/mapper/NoticeMapper.java
@@ -1,6 +1,9 @@
 package kr.or.ddit.mapper;
 
+import java.util.List;
+
 import kr.or.ddit.vo.NoticeVO;
+import kr.or.ddit.vo.PaginationInfoVO;
 
 public interface NoticeMapper {
 
@@ -11,5 +14,11 @@ public interface NoticeMapper {
 	public NoticeVO selectNotice(int boNo);
 
 	public int updateNotice(NoticeVO noticeVO);
+
+	public int deleteNotice(int boNo);
+
+	public int selectNoticeCount(PaginationInfoVO<NoticeVO> pagingVO);
+
+	public List<NoticeVO> selectNoticeList(PaginationInfoVO<NoticeVO> pagingVO);
 
 }
