ed350ac5c5ef5327be6f77765ded96aae54cdb83
sunghee95 <sunghee9647@gmail.com>
2023-06-07 17:20:29 +0900

    페이지 모듈화를 위한 tiles 이용noticeBoard CRUD

--- DevProject/src/main/java/kr/or/ddit/controller/noticeboard/service/INoticeService.java
+++ DevProject/src/main/java/kr/or/ddit/controller/noticeboard/service/INoticeService.java
@@ -1,7 +1,10 @@
 package kr.or.ddit.controller.noticeboard.service;
 
+import java.util.List;
+
 import kr.or.ddit.ServiceResult;
 import kr.or.ddit.vo.NoticeVO;
+import kr.or.ddit.vo.PaginationInfoVO;
 
 public interface INoticeService {
 	public ServiceResult insertNotice(NoticeVO noticeVO);
@@ -9,5 +12,11 @@ public interface INoticeService {
 	public NoticeVO selectNotice(int boNo);
 
 	public ServiceResult updateNotice(NoticeVO noticeVO);
+
+	public ServiceResult deleteNotice(int boNo);
+
+	public int selectNoticeCount(PaginationInfoVO<NoticeVO> pagingVO);
+
+	public List<NoticeVO> selectNoticeList(PaginationInfoVO<NoticeVO> pagingVO);
 
 }
